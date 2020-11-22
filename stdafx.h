980d66e628b2d4ff54e5f6103cf0a182b6efd037
Ola2808-Boro <74494573+Ola2808-Boro@users.noreply.github.com>
2020-11-22 22:19:53 +0100

    Add files via upload

---
+++ stdafx.h
@@ -0,0 +1,25 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include<SFML/Graphics.hpp>
+#include<SFML/Window.hpp>
+#include<SFML/System.hpp>
+#include<SFML/Audio.hpp>
+#include<SFMl/Network.hpp>
+#include<fstream>
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<vector>
+#include<stack>
+#include<map>
+#include <algorithm>
+#include <list>
+#include <string>
+#include <cmath>
+#include <memory>
+#include <utility>
+using namespace std;
+using namespace sf;
+
+#endif // !HEADER_H
