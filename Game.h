17a41afa5b898eb3d1af0f0105b403b763a457c7
Ola2808-Boro <74494573+Ola2808-Boro@users.noreply.github.com>
2020-11-22 22:23:45 +0100

    Update Game.h

--- Game.h
+++ Game.h
@@ -6,21 +6,23 @@
  class Game
 {
 private:
+	
+	GraphicsSettings gfxSettings;
 	RenderWindow *window;
 	Event sfEvent;
-	vector <VideoMode> videomodes;
-	ContextSettings windowSettings;
-	bool fullscreen = false;
+	StateData stateData;
 	Clock dtClock;// speed doesn't depend on frame
 	float dt;
 	stack <State*> states;//stos
 	map<string, int> supportedKeys;//pomocne 
-
+	float gridSize;
 	//init
 	void initVariables();
 	void initWindow();
 	void initStates();
+	void initStateData();
 	void initKeys();
+	void initGraphicsSettings();
 	
 
 public:
