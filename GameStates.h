92e802210e5afdf3717b99db11bac9956389a318
Ola2808-Boro <74494573+Ola2808-Boro@users.noreply.github.com>
2020-11-22 22:21:18 +0100

    Update GameStates.h

--- GameStates.h
+++ GameStates.h
@@ -1,23 +1,33 @@
 #ifndef GAMESTATES_H
 #define GAMESTATES_H
 
-#include"State.h"//dobrze
-#include "Player.h"
+#include "State.h"
+#include "PausedMenu.h"
+#include "TileMap.h"
+
+
 class GameStates:public State//inherit 
 {
 private:
 	Player *player;
+	Font font;
+	PausedMenu *pauseMenu;
+	TileMap tilemap;
 	void initKeybinds();
 	void initTextures();
 	void initPlayers();
+	void InitFonts();
+	void initPauseMenu();
 
 
 public:
-	GameStates(RenderWindow *window, map <string, int>* supportedKeys, stack <State*>* states);
+	GameStates(RenderWindow *window,GraphicsSettings gfxSettings, map <string, int>* supportedKeys, stack <State*>* states);
 	virtual ~GameStates();
 	void update(const float& dt);
 	void render(RenderTarget* target=nullptr);
-	void updateInput(const float& dt);
+	void updatePlayerInput(const float& dt);//W,S,A,D
+	void updateInput(const float dt);//przyciski
+	void updatePauseMenuButtons();
 	Texture temp;//pomoc do zaladowania struktury
 };
 #endif 
