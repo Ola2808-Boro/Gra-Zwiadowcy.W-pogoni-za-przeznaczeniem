34509f2f0c5a740f48cfe4669639689c79d3c6da
Ola2808-Boro <74494573+Ola2808-Boro@users.noreply.github.com>
2020-11-29 10:54:22 +0100

    Update TileMap.cpp

--- TileMap.cpp
+++ TileMap.cpp
@@ -1,35 +1,60 @@
+
 #include"stdafx.h"
 #include "TileMap.h"
 
-TileMap::TileMap()//wartosci domyslne
+TileMap::TileMap(float gridSize, unsigned width, unsigned height)//wartosci domyslne
 {
 	this->gridSizeU = static_cast<unsigned>(gridSizeF);//rzutowanie na unsigned
-	this->gridSizeF = 50;
-	this->maxSize.x = 10;
-	this->maxSize.y =10;
+	this->gridSizeF = gridSize;
+	this->maxSize.x = height;
+	this->maxSize.y =width;
 	this->layers = 1;
-	this->map.resize(maxSize.x);
+	this->map.resize(maxSize.x, vector<vector<Tile*>>());
 	for (size_t x = 0; x < this->maxSize.x; x++)
 	{
-		this->map.resize(maxSize.x);
-		this->map.push_back(vector<vector<Tile>>());
+		//this->map.resize(maxSize.x);
+		//this->map.push_back(vector<vector<Tile*>>());
 		
 		for (size_t y = 0; y < this->maxSize.y; y++)
 		{
-			this->map[x].resize(maxSize.y);
-			this->map[x].push_back(vector<Tile>());
+			this->map[x].resize(maxSize.y, vector<Tile*>());
+			/*this->map[x].push_back(vector<Tile*>());*/
 
 			for (size_t z = 0; z < this->layers; z++)
 			{
-				this->map[x][y].resize(layers);
-				this->map[x][y].push_back(Tile(x * gridSizeF, y * gridSizeF, gridSizeF));
+				this->map[x][y].resize(layers,NULL);
+				/*this->map[x][y].push_back(nullptr);*/
 			}
 		}
+	}
+	if(!this->tileTextureSheet.loadFromFile("Resources/Images/tile/ tilesheet1.png"));
+	{
+		cout << "Error" << endl;
 	}
 }
 
 TileMap::~TileMap()
 {
+	for (size_t x = 0; x < this->maxSize.x; x++)
+	{
+	
+
+		for (size_t y = 0; y < this->maxSize.y; y++)
+		{
+		
+
+			for (size_t z = 0; z < this->layers; z++)
+			{
+				
+				delete this->map[x][y][z];
+			}
+		}
+	}
+}
+
+Texture* TileMap::getTileTextureSheet() 
+{
+	return &this->tileTextureSheet;
 }
 
 void TileMap::update()
@@ -42,10 +67,40 @@ void TileMap::render(RenderTarget& target)
 	{
 		for (auto &y:x) 
 		{
-			for (auto &z : y)
+			for (auto *z : y)
 			{
-				z.render(target);
+				if (z!=NULL)
+				{
+					z->render(target);
+				}
+				
 			}
+		}
+	}
+}
+
+void TileMap::addTile(const unsigned x, const unsigned y, const unsigned z,const IntRect& textureRect)
+{
+	if ((x<this->maxSize.x) &&( x>=0 )&& (y< maxSize.y )&& (y>=0 )&& (z < layers) && (z>=0))//sprawdzi czy spelnia warunki
+	{
+		if (map[x][y][z]==NULL)
+		{
+			this->map[x][y][z] = new Tile(this->gridSizeF*x,  this->gridSizeF*y, this->gridSizeF, this->tileTextureSheet,textureRect);
+			
+		}
+	}
+}
+
+void TileMap::removeTile(const unsigned x, const unsigned y, const unsigned z)
+{
+	if (x < this->maxSize.x && x >= 0 && y < maxSize.y && y >= 0 && z < layers && z >= 0)//sprawdzi czy spelnia warunki
+	{
+		if (map[x][y][z] != NULL)//odwrotnie jak do dodawania
+		{
+			
+			delete this->map[x][y][z];
+			this->map[x][y][z]=NULL;
+
 		}
 	}
 }
