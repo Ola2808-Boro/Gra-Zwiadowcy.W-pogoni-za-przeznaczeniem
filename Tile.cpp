e45732df1e7cd6f4c3b26179b15d039cc97335f6
Ola2808-Boro <74494573+Ola2808-Boro@users.noreply.github.com>
2020-11-29 11:02:39 +0100

    Update Tile.cpp

--- Tile.cpp
+++ Tile.cpp
@@ -5,14 +5,19 @@ Tile::Tile()
 {
 }
 
-Tile::Tile(float x, float y, float gridSizeF)
+Tile::Tile(float x, float y, float gridSizeF,Texture& texture, const IntRect& textureRect)
 {
 	x = 10;
 	y = 10;
 	gridSizeF = gridSizeF;
 	this->shape.setSize(Vector2f(gridSizeF, gridSizeF));
-	this->shape.setFillColor(Color::Green);
+	//this->shape.setFillColor(Color::Green);
+	//->shape.setOutlineThickness(1.f);
+	//this->shape.setOutlineColor(Color::Black);
 	this->shape.setPosition(x, y);
+	this->shape.setTexture(&texture);
+	this->shape.setTextureRect(textureRect);
+	
 }
 
 Tile::~Tile()
