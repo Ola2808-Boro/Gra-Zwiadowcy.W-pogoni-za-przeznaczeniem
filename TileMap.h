3c57b93a16df8715523ab6d3e416c32875526a81
Ola2808-Boro <74494573+Ola2808-Boro@users.noreply.github.com>
2020-11-29 10:55:00 +0100

    Update TileMap.h

--- TileMap.h
+++ TileMap.h
@@ -10,19 +10,23 @@ private:
 	unsigned gridSizeU;
 	unsigned layers;
 	Vector2u maxSize;//unsigned int
-	vector< vector<vector<Tile> > >  map;
+	vector< vector<vector<Tile*> > >  map;
+	Texture tileTextureSheet;
 
 
 public:
 
 	//Konstruktor
-	TileMap();
+	TileMap(float gridSize,unsigned width,unsigned height);
 	//Destruktor
 	virtual~TileMap();
 
+	Texture* getTileTextureSheet() ;//const, zeby bylo bezpieczne
 
 	//Funkcje
 	void update();
 	void render(RenderTarget& target);
+	void addTile(const unsigned x, const unsigned y, const unsigned z, const IntRect& textureRect);
+	void removeTile(const unsigned x, const unsigned y, const unsigned z);
 };
 #endif // !TILEMAP_H
