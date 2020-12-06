bba0f3e6ca75ee0b4f39174f6a95b74978ffab3d
Ola2808-Boro <74494573+Ola2808-Boro@users.noreply.github.com>
2020-12-06 18:45:27 +0100

    Update Entity.cpp

--- Entity.cpp
+++ Entity.cpp
@@ -1,3 +1,4 @@
+#include"stdafx.h"
 #include "Entity.h"
 
 void Entity::initVariables()
@@ -43,12 +44,60 @@ void Entity::createHitboxComponent(Sprite& sprite, float offset_x, float offset_
 	this->hitboxcomponent = new HitboxComponent(this->sprite,offset_x,offset_y,width,height);
 }
 
-void Entity::setPosition(const float x, const float y)
+const FloatRect Entity::getGlobalBounds() const
 {
-		this->sprite.setPosition(x , y);
-	
+	if (this->hitboxcomponent)
+	{
+		return this->hitboxcomponent->getGlobalBounds();;
+	}
+	return this->sprite.getGlobalBounds();
+}
+
+const Vector2f& Entity::getPosition() const
+{
+	if (hitboxcomponent)
+	{
+		return this->hitboxcomponent->getPosition();
+	}
+	return this->sprite.getPosition();
 
 }
+
+const Vector2i Entity::getGridPosition(const int GridSizeI) const
+{
+	if (this->hitboxcomponent)
+	{
+		return Vector2i(static_cast<int>(this->hitboxcomponent->getPosition().x / GridSizeI), static_cast<int>(this->hitboxcomponent->getPosition().y / GridSizeI));
+	}
+	else
+	{
+		return Vector2i(static_cast<int>(this->sprite.getPosition().x / GridSizeI), static_cast<int>(this->sprite.getPosition().y / GridSizeI));
+
+	}
+}
+
+void Entity::setPosition(const float x, const float y)
+{
+	if (hitboxcomponent)
+	{
+		this->hitboxcomponent->setPosition(x, y);
+	}
+	else
+	{
+		this->sprite.setPosition(x, y);
+	}
+
+}
+
+const FloatRect Entity::getNextPositionBounds(const float& dt) const
+{
+	if (this->hitboxcomponent && this->movementComponent)
+		return this->hitboxcomponent->getNextPosition(this->movementComponent->getVelocity() * dt);
+
+	return sf::FloatRect(-1.f, -1.f, -1.f, -1.f);
+}
+
+
 
 void Entity::update(const float& dt)//na strzalki
 {
@@ -58,13 +107,37 @@ void Entity::update(const float& dt)//na strzalki
 void Entity::render(RenderTarget &target)
 {
 	
-		target.draw(this->sprite);
-		if (this->hitboxcomponent)//jezeli istnieje
-		{
-			hitboxcomponent->render(target);
+		//target.draw(this->sprite);
+		//if (this->hitboxcomponent)//jezeli istnieje
+		//{
+		//	hitboxcomponent->render(target);
 
-		}
+		//}
 	
+}
+
+void Entity::stopVelocity()
+{
+	if (this->movementComponent)//jezeli itnieje
+	{
+		this->movementComponent->stopVelocity();
+	}
+}
+
+void Entity::stopVelocityX()
+{
+	if (this->movementComponent)//jezeli itnieje
+	{
+		this->movementComponent->stopVelocityX();
+	}
+}
+
+void Entity::stopVelocityY()
+{
+	if (this->movementComponent)//jezeli itnieje
+	{
+		this->movementComponent->stopVelocityY();
+	}
 }
 
 void Entity::move(const float direction_x, const float  direction_y, const float& dt )
@@ -75,5 +148,6 @@ void Entity::move(const float direction_x, const float  direction_y, const float
 		this->movementComponent->move(direction_x, direction_y,dt);
 		
 	}
+	
 }
 
