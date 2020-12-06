e16605e2be6a2e606e392ffdee265f1ffac714d6
Ola2808-Boro <74494573+Ola2808-Boro@users.noreply.github.com>
2020-12-06 18:49:38 +0100

    Update GameStates.h

--- GameStates.h
+++ GameStates.h
@@ -5,25 +5,40 @@
 #include "PausedMenu.h"
 #include "TileMap.h"
 
+//class PausedMenu;
+//class Player;
+//class TileMap;
+//class View;
+//class Font;
+//class RenderTexture;
 
 class GameStates:public State//inherit 
 {
 private:
+	View view;
+	RenderTexture renderTexture;
+	Sprite renderSprite;
 	Player *player;
 	Font font;
 	PausedMenu *pauseMenu;
-	TileMap tilemap;
+	TileMap *tileMap;
+
+	void iniDefferedRender();
+	void initView();
 	void initKeybinds();
 	void initTextures();
 	void initPlayers();
 	void InitFonts();
 	void initPauseMenu();
+	void initTileMap();
 
 
 public:
-	GameStates(RenderWindow *window,GraphicsSettings gfxSettings, map <string, int>* supportedKeys, stack <State*>* states);
+	GameStates(StateData* stateData);
 	virtual ~GameStates();
+	void updateView(const float& dt);
 	void update(const float& dt);
+	void updateTileMap(const float& dt);
 	void render(RenderTarget* target=nullptr);
 	void updatePlayerInput(const float& dt);//W,S,A,D
 	void updateInput(const float dt);//przyciski
