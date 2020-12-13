4e811923df81ca6b732c19455edc2d9188b7b759
Ola2808-Boro <74494573+Ola2808-Boro@users.noreply.github.com>
2020-12-13 22:30:09 +0100

    Update AnimationComponent.cpp

--- AnimationComponent.cpp
+++ AnimationComponent.cpp
@@ -1,143 +1,126 @@
-#ifndef ANIMATIONCOMPONENT_H
-#define ANIMATIONCOMPONENT_H
+#include"stdafx.h"
+#include "AnimationComponent.h"
 
-//#include <iostream>
-//#include<map>
-//#include<SFML/Graphics.hpp>
-//#include<SFML/Window.hpp>
-//#include<SFML/System.hpp>
-//#include<SFML/Audio.hpp>
-//#include<SFMl/Network.hpp>
-using namespace std;
-using namespace sf;
-class AnimationComponent
+AnimationComponent::AnimationComponent(Sprite &sprite,Texture& texture_sheet):sprite(sprite), textureSheet(texture_sheet),lastAnimation(NULL), priorityanimation(NULL)
 {
-private:
-	class Animation
+}
+
+AnimationComponent::~AnimationComponent()
+{
+	for (auto &i :this->animation )
 	{
-	public:
-		Sprite& sprite;
-		Texture& textureSheet;
-		float animationTimer;
-		float timer;
-		bool done;
-		int width,height;
-		IntRect startRect;
-		IntRect endRect;
-		IntRect currentRect;
-		Animation(Sprite& sprite,Texture& textureSheet, float animation_timer,int start_frame_x,int start_frame_y,int end_frame_x,int end_frame_y,int width,int height)
-			:sprite(sprite),textureSheet(textureSheet),timer(0.f),done(false) ,animationTimer(animationTimer),width(width),height(height)
-		{
-			timer = 0.f;
-			this->startRect = IntRect(start_frame_x*width, start_frame_y*width, width, height);
-			currentRect = startRect;
-			this->endRect = IntRect(end_frame_x * height, end_frame_y*height, width, height);
-			this->sprite.setTexture(textureSheet);
-			this->sprite.setTextureRect(startRect);// Ustawia rozmiar widocznej części rysowanej tekstury
-		}
-		~Animation();
-		const bool& isDone()const
-		{
-			return done;
-		}
-		const bool &play(const float& dt)
-		{
-			
-			//Update timer
-			this->timer += 100.f * dt;
-			if (this->timer >= this->animationTimer)
-			{
-				this-> done = false;
-				//reset timer
-				this->timer = 0.f;
-				cout << "Wszedles do const play Animation" << endl;
-				//Animate
-				if (this->currentRect != this->endRect)
-				{
-					cout << "Wszedles do this->currentRect != this->endRect" << endl;
-					this->currentRect.left += this->width;
-				}
-				else //Reset
-				{
-					cout << "Wszedles do this->currentRect == this->endRect" << endl;
-					this->currentRect.left = this->startRect.left;//cofam
-					this->done = true;
-				}
+		delete i.second;
 
-				this->sprite.setTextureRect(this->currentRect);
-			}
+	}
+}
 
-			return this->done;
-		}
-		const bool& play(const float& dt,float mod_percent)
+const bool& AnimationComponent::isDone(const string key) 
+{
+	return this->animation[key]->isDone();
+
+}
+
+const bool& AnimationComponent::play(const string key,const float& dt,const bool priority)
+{
+	if (this->priorityanimation) //jezeli istnieje to
+	{
+		if (this->priorityanimation == this->animation[key])
 		{
-			this->done = false;
-			if (mod_percent<0.5f)
+			if (this->lastAnimation != this->animation[key])
 			{
-				mod_percent = 0.5f;//ustam min wartosc
-			}
-			timer +=mod_percent*dt*100.f ;
-			if (timer>=animationTimer)
-			{
-				timer = 0.f;//resetuje timer
-				if (currentRect!=endRect)
-				{
-					currentRect.left = currentRect.left + width;
-				}
+				if (this->lastAnimation == NULL)
+					this->lastAnimation = this->animation[key];
 				else
 				{
-					currentRect.left = startRect.left;
-					this->done = true;
+					this->lastAnimation->reset();
+					this->lastAnimation = this->animation[key];
 				}
-				this->sprite.setTextureRect(currentRect);
 			}
-			return this->done;
+
+			//jezeli jest done to usun
+			if (this->animation[key]->play(dt))
+			{
+				this->priorityanimation = NULL;
+			}
 		}
-		void reset()
+	}
+	else //Play animation of no other priority animation is set
+	{
+		//If this is a priority animation, set it.
+		if (priority)
 		{
-			timer = 0.f;
-			currentRect = startRect;
+			this->priorityanimation = this->animation[key];
 		}
-		void pause();
 
-	};
-	map <string, Animation*> animation;//spojrz na clase Animation
-	Sprite& sprite;
-	Texture& textureSheet;
-	Animation* lastAnimation;
-	Animation* priorityanimation;//pryjotetowa animacja 
-public:
+		if (this->lastAnimation != this->animation[key])
+		{
+			if (this->lastAnimation == NULL)
+				this->lastAnimation = this->animation[key];
+			else
+			{
+				this->lastAnimation->reset();
+				this->lastAnimation = this->animation[key];
+			}
+		}
 
-	AnimationComponent(Sprite& sprite, Texture &texture_sheet);
-	virtual ~AnimationComponent();
-	const bool &isDone(const string key);
-	const bool& play(const string key,const float &dt, const bool priority=false);
-	const bool& play(const string key,const float &dt,const float &modifier,const float &modifier_max, const bool priority = false);
-	void addAnimation(const string key, float animation_timer, int start_frame_x, int start_frame_y, int end_frame_x, int end_frame_y, int width, int height);
+		this->animation[key]->play(dt);
+	}
+	return this->animation[key]->isDone();
+	
+}
+const bool& AnimationComponent::play(const string key, const float& dt,const float& modifier, const float& modifier_max, const bool priority )
+{
+	if (this->priorityanimation) //If there is a priority animation
+	{
+		if (this->priorityanimation == this->animation[key])
+		{
+			if (this->lastAnimation != this->animation[key])
+			{
+				if (this->lastAnimation == NULL)
+					this->lastAnimation = this->animation[key];
+				else
+				{
+					this->lastAnimation->reset();
+					this->lastAnimation = this->animation[key];
+				}
+			}
 
-};
+			//If the priority animation is done, remove it
+			if (this->animation[key]->play(dt, abs(modifier / modifier_max)))
+			{
+				this->priorityanimation = NULL;
+			}
+		}
+	}
+	else //Play animation of no other priority animation is set
+	{
+		//If this is a priority animation, set it.
+		if (priority)
+		{
+			this->priorityanimation = this->animation[key];
+		}
 
+		if (this->lastAnimation != this->animation[key])
+		{
+			if (this->lastAnimation == NULL)
+				this->lastAnimation = this->animation[key];
+			else
+			{
+				this->lastAnimation->reset();
+				this->lastAnimation = this->animation[key];
+			}
+		}
 
+		this->animation[key]->play(dt, abs(modifier / modifier_max));
+	}
+	return this->animation[key]->isDone();
+}
 
+void AnimationComponent::addAnimation(const string key,float animation_timer, int start_frame_x, int start_frame_y, int end_frame_x, int end_frame_y, int width, int height)
+{
+	this->animation[key] = new Animation(sprite, textureSheet, animation_timer, start_frame_x, start_frame_y, end_frame_x, end_frame_y, width, height);
+}
 
-#endif // !ANIMATIONCOMPONENT_H
-
-
-//Klasa Rect
-//Klasa narzędziowa do manipulowania prostokątami wyrównanymi w osi 2D.
-//
-//Prostokąt jest definiowany przez jego lewy górny róg i jego rozmiar.
-
-// Define a rectangle, located at (0, 0) with a size of 20x5
-//sf::IntRect r1(0, 0, 20, 5);
-//// Define another rectangle, located at (4, 2) with a size of 18x10
-//sf::Vector2i position(4, 2);
-//sf::Vector2i size(18, 10);
-//sf::IntRect r2(position, size);
-//// Test intersections with the point (3, 1)
-//bool b1 = r1.contains(3, 1); // true
-//bool b2 = r2.contains(3, 1); // false
-//// Test the intersection between r1 and r2
-//sf::IntRect result;
-//bool b3 = r1.intersects(r2, result); // true
-//// result == (4, 2, 16, 3)
+AnimationComponent::Animation::~Animation()
+{
+}
