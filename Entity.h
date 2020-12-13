ded58de127b7e15cae6c00f26d000e7a0827bb8a
Ola2808-Boro <74494573+Ola2808-Boro@users.noreply.github.com>
2020-12-13 22:37:12 +0100

    Update Entity.h

--- Entity.h
+++ Entity.h
@@ -5,10 +5,12 @@
 #include "HitboxComponent.h"
 #include "MovementComponent.h"
 #include "AnimationComponent.h"
+#include "AttributeComponent.h"
 
 class HitboxComponent;
 class MovementComponent;
 class AnimationComponent;
+class AttributeComponent;
 
 class Entity
 {
@@ -23,6 +25,7 @@ protected:
 	MovementComponent * movementComponent;
 	AnimationComponent* animationcomponent;
 	HitboxComponent* hitboxcomponent;
+	AttributeComponent* attributeComponent;
 public:
 	Entity();
 	virtual ~Entity();
@@ -31,6 +34,7 @@ public:
 	void createMovementComponent(const float maxVelocity, const float acceleration, float deceleration);
 	void createAnimationComponent(Texture& texture_sheet);
 	void createHitboxComponent(Sprite &sprite, float offset_x, float offset_y, float width, float height);
+	void creatAttributeComponent(const int level);
 
 	virtual const FloatRect getGlobalBounds()const;
 	virtual const Vector2f& getPosition()const;
