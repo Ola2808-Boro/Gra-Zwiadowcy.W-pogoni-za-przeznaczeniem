254b45133947d0e919d5830ddaef80b28d987f24
Ola2808-Boro <74494573+Ola2808-Boro@users.noreply.github.com>
2020-12-20 21:37:22 +0100

    Update Player.h

--- Player.h
+++ Player.h
@@ -2,16 +2,18 @@
 #define  PLAYER_H
 
 #include "Entity.h"//donrze
+#include "Sword.h"
 class Entity;
 
-class Player :
-    public Entity
+class Player :public Entity
 {
 private:
     void initVariables();
     void initComponents();
+    void initAnimation();// beda znajdowaly sie to funkcje dotyczace ruchu postaci 
     bool attacking;
-
+    Sword *sword;
+  
    
 public:
     Player(float x, float y, Texture& texture_sheet);
@@ -19,10 +21,12 @@ public:
 
     AttributeComponent* getAttributeComponent();
 
-    void update(const float& dt);
-    void render(RenderTarget& target);
+    void update(const float& dt,Vector2f & mouse_pos_view);//wszytskie position sa w statec
+    void render(RenderTarget& target, Shader* shader = NULL, const Vector2f light_position = Vector2f(), const bool show_hitbox = false);
+ 
     void updateAnimation(const float& dt);
     void updateAttack();
+
     void loseHp(const int hp);
     void gainHp(const int hp);
     void loseExp(const int exp);
