289792cd83e1a0b1e0f4904e72e281b565d33d6f
Ola2808-Boro <74494573+Ola2808-Boro@users.noreply.github.com>
2020-12-20 21:44:49 +0100

    Update Gui.cpp

--- Gui.cpp
+++ Gui.cpp
@@ -1,6 +1,19 @@
 #include"stdafx.h"
 #include "Gui.h"
 
+
+const float  gui::p2pX(const float perc, const VideoMode& vm)//konwersja procentow na pixele
+{
+	return floor(static_cast<float>(vm.width) * (perc / 100.f));
+}
+const float gui::p2pY(const float perc, const VideoMode& vm)
+{
+	return floor(static_cast<float>(vm.height) * (perc / 100.f));
+}
+const unsigned gui::calucuateCharacterSize(const VideoMode& vm, const unsigned modifier)
+{
+	return static_cast<unsigned>((vm.width + vm.height) / modifier);
+}
 gui::Button::Button(float x, float y, float width, float height, string text_button, Font font_button, Color hoverColor, Color activeColor, Color idleColor, Color outlinehoverColor, Color outlineidleColor,
 	Color outlineactiveColor, unsigned character_size, Color text_idle, Color text_hover, Color text_active, short unsigned id)
 
@@ -62,12 +75,12 @@ const string gui::Button::getText()const
 	return this->text.getString();
 }
 
-const short unsigned& gui::Button::getId() const//zwracam id 
+const short unsigned& gui::Button::getId() const
 {
 	return this->id;
 }
 
-const unsigned short & gui::DropDownList::getActiveElementId() const
+const unsigned short& gui::DropDownList::getActiveElementId() const
 {
 	return this->activeElement->getId();//wyrzuca blad
 }
@@ -134,8 +147,8 @@ void gui::Button::update(Vector2i& mousePostWindow)
 
 gui::DropDownList::DropDownList(float x, float y, float width, float height, string list[], Font& font, unsigned nrOfElements, unsigned default_index ):font(font),showList(showList),keyTimeMax(1.f),keyTime(0.f)//domysle wartosci
 {
-	this->showList = showList;
-	this->activeElement = new gui::Button(x, y, width, height, list[default_index], this->font, Color::Blue, Color::Cyan, Color::Yellow, Color::Red, Color::Black, Color::Blue, 12, Color::Red, Color::Yellow, Color::White,0);
+	this->showList = false;//tu byl blad, bo to bylo bool , a ja miałam showlist
+	this->activeElement = new gui::Button(x, y, width, height, list[default_index], this->font, Color::Blue, Color::Cyan, Color::Yellow, Color::Red, Color::Black, Color::Blue, 12, Color::Red, Color::Yellow, Color::White);
 	//unsigned nrOfElement = sizeof(list) / sizeof(string);//ile lementow jest w liscie
 	for (unsigned i = 0; i < nrOfElements; i++)
 	{
@@ -183,17 +196,17 @@ void gui::DropDownList::update(Vector2i& mousePostWindow, const float& dt)
 		}
 		
 	}
-	if (showList)//jezeli istnieje
+	if (this->showList)
 	{
 		for (auto& i : this->list)
 		{
 			i->update(mousePostWindow);
 
-			if (i->isPressed()&& getKeyTime())
+			if (i->isPressed() && this->getKeyTime())
 			{
-				 this->showList = false;
-				 this->activeElement->setText(i->getText());
-				 this->activeElement->setId(i->getId());
+				this->showList = false;
+				this->activeElement->setText(i->getText());
+				this->activeElement->setId(i->getId());
 			}
 		}
 	}
@@ -226,15 +239,15 @@ gui::TextureSelector::TextureSelector(float x, float y, float width, float heigh
 	this->gridSize = gridSize;
 	this->active = false;
 	this->hidden = false;
-	float offest = 100.f;
-	this->bounds.setPosition(x+offest, y);
+	float offset = gridSize;
+	this->bounds.setPosition(x+ offset, y);
 	this->bounds.setSize(Vector2f(width, height));
 	this->bounds.setFillColor(Color(50, 50, 50, 100));
 	this->bounds.setOutlineThickness(1.f);
 	this->bounds.setOutlineColor(Color(255, 255, 255, 200));
 
 	this->sheet.setTexture(*textureSheet);
-	this->sheet.setPosition(x+offest, y);
+	this->sheet.setPosition(x+ offset, y);
 
 	if (this->sheet.getGlobalBounds().width>bounds.getGlobalBounds().width)
 	{
@@ -253,7 +266,7 @@ gui::TextureSelector::TextureSelector(float x, float y, float width, float heigh
 	this->texuretRect.width = static_cast<int>(gridSize);
 	this->texuretRect.height = static_cast<int>(gridSize);
 
-	this->hiddenButton =  new gui::Button(x,y, 50, 50, text, font, Color::Blue, Color::Cyan, Color::Yellow, Color::Yellow, Color::Red, Color::Black, 30, Color::Red, Color::Yellow, Color::White,0);
+	this->hiddenButton =  new gui::Button(x,y, 50, 50, text, font, Color::Blue, Color::Cyan, Color::Yellow, Color::Yellow, Color::Red, Color::Black, 30, Color::Red, Color::Yellow, Color::White);
 }
 
 gui::TextureSelector::~TextureSelector()
@@ -264,7 +277,7 @@ gui::TextureSelector::~TextureSelector()
 
 void gui::TextureSelector::update(Vector2i& mousePosWindow,const float &dt)
 {
-
+	this->updateKeyTime(dt);
 	this->hiddenButton->update(mousePosWindow);
 	if (this->hiddenButton->isPressed()&& getKeyTime())
 	{
@@ -279,27 +292,24 @@ void gui::TextureSelector::update(Vector2i& mousePosWindow,const float &dt)
 	}
 	if (!this->hidden)
 	{
-		if (this->bounds.getGlobalBounds().contains(static_cast<Vector2f>(mousePosWindow)))
+		this->active = false;
+
+		if (this->bounds.getGlobalBounds().contains(static_cast<sf::Vector2f>(mousePosWindow)))
 		{
 			this->active = true;
-		}
-		else
-		{
-			this->active = false;
-		}
 
-		if (this->active)
-		{
-			this->mousePosGrid.x = ((mousePosWindow.x - static_cast<int>(this->bounds.getPosition().x)) / static_cast<unsigned>(gridSize));
-			this->mousePosGrid.x = ((mousePosWindow.y - static_cast<int>(this->bounds.getPosition().y)) / static_cast<unsigned>(gridSize));
-			this->selector.setPosition(this->bounds.getPosition().x + mousePosGrid.x * this->gridSize, this->bounds.getPosition().y + mousePosGrid.y * this->gridSize);
-		}
-		this->texuretRect.left = static_cast<int>(this->selector.getPosition().x - this->bounds.getPosition().x);
-		this->texuretRect.top = static_cast<int>(this->selector.getPosition().y - this->bounds.getPosition().y);
-	}
-	else
-	{
+			this->mousePosGrid.x = (mousePosWindow.x - static_cast<int>(this->bounds.getPosition().x)) / static_cast<unsigned>(this->gridSize);
+			this->mousePosGrid.y = (mousePosWindow.y - static_cast<int>(this->bounds.getPosition().y)) / static_cast<unsigned>(this->gridSize);
 
+			this->selector.setPosition(
+				this->bounds.getPosition().x + this->mousePosGrid.x * this->gridSize,
+				this->bounds.getPosition().y + this->mousePosGrid.y * this->gridSize
+			);
+
+			//Update texture rectangle
+			this->texuretRect.left = static_cast<int>(this->selector.getPosition().x - this->bounds.getPosition().x);
+			this->texuretRect.top = static_cast<int>(this->selector.getPosition().y - this->bounds.getPosition().y);
+		}
 	}
 
 }
@@ -345,6 +355,52 @@ void gui::TextureSelector::updateKeyTime(const float& dt)
 		keyTime += dt * 10;
 	}
 }
+//--------------------------------------------------ProgressBar--------------------------------------------------
+gui::ProgressBar::ProgressBar(float new_x, float new_y, float new_width, float new_height, int max_value, Color inner_color, unsigned character_size, Font* font, VideoMode& vm)
+{
+	float width = gui::p2pX(new_width,vm);
+	float height = gui::p2pY(new_height, vm);
+	float x = gui::p2pX(new_x,vm);
+	float y = gui::p2pY(new_y,vm);
+	this->progressBarMaxSizeWidth = width;
+	this->maxValue = max_value;
+
+
+	this->progressBarBack.setSize(Vector2f(width, height));
+	this->progressBarBack.setFillColor(Color(50, 50, 50, 200));
+	this->progressBarBack.setPosition(x, y);
+	if (font)
+	{
+		this->progressBarText.setFont(*font);
+		this->progressBarText.setCharacterSize(gui::calucuateCharacterSize(vm, 180));
+		this->progressBarInner.setSize(Vector2f(width, height));
+		this->progressBarInner.setFillColor(Color(250, 20, 20, 200));
+		this->progressBarInner.setPosition(this->progressBarBack.getPosition());
+	}
+
+
+	this->progressBarText.setPosition(this->progressBarBack.getPosition());
+}
+
+gui::ProgressBar::~ProgressBar()
+{
+}
+
+void gui::ProgressBar::updateProgressBar(const int current_value)
+{
+	float percent = static_cast<float>(current_value / static_cast<float>(this->maxValue));
+	this->progressBarInner.setSize(Vector2f(static_cast<float>(floor(this->progressBarMaxSizeWidth * percent)), this->progressBarInner.getSize().y));
+	this->progressBarString = to_string(current_value) + " / " + to_string(maxValue);
+	this->progressBarText.setString(this->progressBarString);
+}
+
+void gui::ProgressBar::renderProgressBar(RenderTarget& target)
+{
+	target.draw(progressBarBack);
+	target.draw(progressBarInner);
+	target.draw(progressBarText);
+}
+
 
 
 
