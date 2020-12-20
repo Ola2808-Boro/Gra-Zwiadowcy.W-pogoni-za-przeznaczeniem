2fb79f087918c3186bb87f4cdf0140b948f2a021
Ola2808-Boro <74494573+Ola2808-Boro@users.noreply.github.com>
2020-12-20 21:47:36 +0100

    Update MainMenuState.h

--- MainMenuState.h
+++ MainMenuState.h
@@ -10,16 +10,16 @@ class MainMenuState:public State //dziedzicze po State ma f wirtualne
 {
 private:
 	RectangleShape background;//tlo
+	RectangleShape btnBackground;//tlo dla przyciskow
 	Font font;
 	Texture backgroundtexture;//zeby miec funkcje z tej klasy
 	//gui::Button *gamestate_button;//obiekt klasy zajmujacej sie prostokatem
 	map <string, gui::Button*> buttons;
 	void initKeybinds();
 	void InitFonts();
-	void initButtom();
-	void initBackground();
+	void initGui();
 	void initVariables();
-
+	void resetGui();
 public:
 	MainMenuState(StateData* stateData);
 	void update(const float& dt);
