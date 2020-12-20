b616e840ec3e549587eb707ef62fd47e84f4b99b
Ola2808-Boro <74494573+Ola2808-Boro@users.noreply.github.com>
2020-12-20 21:35:06 +0100

    Update TileMap.h

--- TileMap.h
+++ TileMap.h
@@ -31,21 +31,25 @@ public:
 
 	//Konstruktor
 	TileMap(float gridSize, int width,int height, string texture_file);
+	TileMap(const string file_name);
 	//Destruktor
 	virtual~TileMap();
 
+	const bool tileEmpty(const int x, const int y, const int z) const;
 	const Texture* getTileTextureSheet() const ;//const, zeby bylo bezpieczne
 	const int getLayerSize(const int x, const int y, const int layer) const;
+	const Vector2f& getMaxSizeF() const;
 
 	//Funkcje
-	void update();
-	void render(RenderTarget& target,const Vector2i& gridPosition);
-	void renderDeferred(RenderTarget& target);
+	void render(RenderTarget& target, const Vector2i& gridPosition, Shader* shader = NULL, const Vector2f playerPosition = Vector2f(), const bool show_collision=false);
+	void renderDeferred(RenderTarget& target,Shader* shader = NULL, const Vector2f playerPosition = Vector2f());
 	void addTile(const int x, const int y, const int z, const IntRect& texture_rect,const bool& collision,const short& type);
 	void removeTile(const int x, const int y, const int z);
 	void saveToFile(const string file_name);
 	void loadFromFile(const string file_name);
-	void updateCollision(Entity * entity, const float& dt);
+	void update(Entity * entity, const float& dt);
+	const bool checkType(const int x, const int y, const int z, const int type) const;
+
 
 };
 #endif // !TILEMAP_H
