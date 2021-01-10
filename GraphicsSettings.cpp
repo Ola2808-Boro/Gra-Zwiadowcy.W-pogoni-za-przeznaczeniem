28eb86936c4d9d657f3af501587db32b5460426e
Ola2808-Boro <74494573+Ola2808-Boro@users.noreply.github.com>
2021-01-10 16:46:46 +0100

    Update GraphicsSettings.cpp

--- GraphicsSettings.cpp
+++ GraphicsSettings.cpp
@@ -1,18 +1,18 @@
 #include "stdafx.h"
 #include "GraphicsSettings.h"
 
+//-----------------------------------------------------Konstruktor-------------------------------------------//
 GraphicsSettings::GraphicsSettings()
 {
 	this->title = "Ola";
 	this->resolutions = VideoMode::getDesktopMode();
-	this->fullscreen = false;
+	this->fullscreen = true;
 	this->verticalSync = false;
-	this->verticalSync1 = true;
 	contextSettings.antialiasingLevel = 0;
 	this->frameRateLimit = 120;
 	videModes = VideoMode::getFullscreenModes();
 }
-
+//-------------------------------------------------Funkcje-----------------------------------------------//
 void GraphicsSettings::saveToFile(string path)
 	{
 		ofstream ofs(path);
@@ -23,12 +23,11 @@ void GraphicsSettings::saveToFile(string path)
 			ofs << fullscreen;
 			ofs << frameRateLimit;
 			ofs << verticalSync;
-			ofs << verticalSync1;
 			ofs << contextSettings.antialiasingLevel;
 
 		}
 		ofs.clear();
-		ofs.seekp(0);//beginning
+		ofs.seekp(0);//ustaw na poczatek
 		ofs.close();
 	}
 void GraphicsSettings::loadToFile(string path)
@@ -41,11 +40,10 @@ void GraphicsSettings::loadToFile(string path)
 			ifs >> fullscreen;
 			ifs >> frameRateLimit;
 			ifs >> verticalSync;
-			ifs >> verticalSync1;
 			ifs >> contextSettings.antialiasingLevel;
 
 		}
 		ifs.clear();
-		ifs.seekg(0);//beginning
+		ifs.seekg(0);//ustaw na poczatek
 		ifs.close();
 	}
