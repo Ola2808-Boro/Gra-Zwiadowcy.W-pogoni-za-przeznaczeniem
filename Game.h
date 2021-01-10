106a941529698948226068a405e9008a3f51cc44
Ola2808-Boro <74494573+Ola2808-Boro@users.noreply.github.com>
2021-01-10 16:44:50 +0100

    Update Game.h

--- Game.h
+++ Game.h
@@ -13,10 +13,11 @@ private:
 	StateData stateData;
 	Clock dtClock;// speed doesn't depend on frame
 	float dt;
-	stack <State*> states;//stos
+	stack <State*> states;//stos, w którym bede przechowywałam obiekty klasy State
 	map<string, int> supportedKeys;//pomocne 
 	float gridSize;
-	//init
+
+	//init 6
 	void initVariables();
 	void initWindow();
 	void initStates();
@@ -26,18 +27,23 @@ private:
 	
 
 public:
+	//konstruktor
 	Game();
+	//destruktor
 	virtual ~Game();
+	//funkcje update
 	void updateSFMLEvent();
 	void update();
-	void run();
+	void updateDT();
+
+	//funkcje render
 	void render();
+
+	//funckje run i end
 	void endApplication();
-	void updateDT();//
+	void run();
 
 
 };
 #endif 
-
-
 
