b3fc21551bea30613c8f6fdb3c0babffd4ddc92f
Ola2808-Boro <74494573+Ola2808-Boro@users.noreply.github.com>
2021-01-10 16:42:12 +0100

    Update AttributeComponent.h

--- AttributeComponent.h
+++ AttributeComponent.h
@@ -5,11 +5,14 @@
 class AttributeComponent
 {
 public:
+	
 	//level
 	int level;
 	int exp;
 	int expNext;
 	int attributePoints;
+	Text gameover;
+	Font font;
 
 	//atrybuty
 	int vitality;
@@ -26,19 +29,29 @@ public:
 	int accuracy;
 	int defence;
 	int luck;
+
 	//konstruktor
 	AttributeComponent(int level);
 	//destruktor
 	virtual~AttributeComponent();
 	//funkcje
 	string debugPrint()const;
+	const bool isDead()const;
+	//funckje update
 	void updateStats(const bool reset);
 	void updateLevel();
 	void update();//glowna
 
-	void gainExp(const int exp);
+	//funckje Hp
 	void loseHp(const int hp);
 	void gainHp(const int hp);
+	//funckje Exp
+	void gainExp(const int exp);
 	void loseExp(const int exp);
+
+	void render(RenderTarget& target);
+
+	
+
 };
 #endif // !ATTRIBUTTECOMPONENT
