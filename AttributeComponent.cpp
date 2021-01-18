db984ee0640f40c2d0eb0149438443fb44b4629d
Ola2808-Boro <74494573+Ola2808-Boro@users.noreply.github.com>
2021-01-18 11:51:26 +0100

    Update AttributeComponent.cpp

--- AttributeComponent.cpp
+++ AttributeComponent.cpp
@@ -3,8 +3,12 @@
 //-----------------------------KONSTRUKTOR----------------------------------------//
 AttributeComponent::AttributeComponent(int level)
 {
-	this->level = level;;
-	this->exp=0;
+	this->loadFromFileGui("Config/saveGui.txt");
+	this->loadFromFile("Config/saveGame.txt");
+
+	this->level = level;
+	this->exp = 0;
+
 	this->expNext = (50 * pow(this->level, 3) - 150 * pow(this->level, 2) + (UINT64)400 * this->level) / 3;
 	this->attributePoints=2;
 
@@ -14,8 +18,9 @@ AttributeComponent::AttributeComponent(int level)
 	this->agility = 1;
 	this->intelligence = 1;
 
-	this->updateLevel();
+	
 	this->updateStats(true);
+	this->updateLevel();
 }
 //----------------------------------DESTRUKTOR----------------------------------------//
 AttributeComponent::~AttributeComponent()
@@ -30,16 +35,37 @@ string AttributeComponent::debugPrint() const
 //----------------------------FUNKCJE----------------------------------------------//
 void AttributeComponent::updateStats(const bool reset)
 {
-	this->hpMax = this->vitality * 5 + this->vitality + this->strength / 2 + this->intelligence / 5;
-	this->damageMin = this->strength * 2 + this->strength / 4 + this->intelligence / 5;
+	if (prawda_czy_falsz==1)
+	{
+		this->exp = playerExp;
+		this->hp = playerHp;
+		this->level = levelSave;
+		this->hpMax = this->vitality * 5 + this->vitality + this->strength / 2 + this->intelligence / 5;
+		this->hpMaxEnemy = this->vitality * 5 + this->vitality + this->strength / 2 + this->intelligence / 5;
+	}
+	else
+	{
+		this->hpMax = this->vitality * 5 + this->vitality + this->strength / 2 + this->intelligence / 5;
+		this->hpMaxEnemy = this->vitality * 5 + this->vitality + this->strength / 2 + this->intelligence / 5;
+	}
+	this->damageMin = this->strength/6  + this->strength / 6 + this->intelligence / 6;
 	this->damageMax = this->strength * 2 + this->strength / 2 + this->intelligence / 5;
 	this->accuracy = this->dexterity * 5 + this->dexterity / 2 + this->intelligence / 5;
 	this->defence = this->agility * 2 + this->agility / 4 + this->intelligence / 5;
 	this->luck = this->intelligence * 2 + this->intelligence / 5;
 	if (reset)
 	{
-		this->hp = this->hpMax;
+		if (prawda_czy_falsz == 1)
+		{
+
+		}
+		else
+		{
+			this->hp = this->hpMax;
+			this->hpEnemy = this->hpMaxEnemy;
+		}
 	}
+	cout << "Level wynosi" << " " << level << endl;
 }
 
 
@@ -47,6 +73,7 @@ void AttributeComponent::updateLevel()
 {
 	while (this->exp>=this->expNext)//dopoki to obecne exp jest wieksze rowne temu nastpnemu, to zwieksze level
 	{
+		
 		level++;
 		this->exp -= this->expNext;
 		this->expNext = (50 * pow(this->level, 3) - 150 * pow(this->level, 2) + (UINT64)400 * this->level) / 3;;
@@ -68,13 +95,15 @@ void AttributeComponent::gainExp(const int exp)
 
 void AttributeComponent::loseHp(const int hp)
 {
+
 	this->hp -= hp;
 	if (this->hp < 0)
 	{
-		this->hp = 0;
-		
-		
-	}
+			this->hp = 0;
+
+
+		}
+	
 }
 
 void AttributeComponent::gainHp(const int hp)
@@ -104,3 +133,36 @@ const bool AttributeComponent::isDead() const
 {
 	return this->hp<=0;
 }
+void AttributeComponent::loadFromFile(string path)
+{
+	load = true;
+	ifstream ifs(path);
+	if (ifs.is_open())
+	{
+
+		ifs >> playerPosition_x;
+		ifs >> playerPosition_y;
+		ifs >> playerHp;
+		ifs >> playerExp;
+		ifs >> enemyPosition_x;
+		ifs >> enemyPosition_y;
+		ifs >> enemyHp;
+		ifs >> enemyExp;
+		ifs >> saveNumber;
+		ifs >> levelSave;
+	}
+	ifs.clear();
+	ifs.seekg(0);//beginning
+	ifs.close();
+
+}
+void AttributeComponent::loadFromFileGui(string path)
+{
+	ifstream ifs(path);
+	if (ifs.is_open())
+	{
+		ifs >> prawda_czy_falsz;
+	}
+	ifs.clear();
+	ifs.seekg(0);//beginning
+	ifs.close();
